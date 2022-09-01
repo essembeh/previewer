63312ae86a909fd1d3328a788f16be9f258a4a99
Sébastien MB <seb@essembeh.org>
2022-09-01 21:22:42 +0200

    🔨 Allow skipping download for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from os import getenv
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from urllib.request import urlretrieve
@@ -15,23 +16,24 @@ def download(url: str, file: Path) -> Path:
 def pytest_sessionstart(session):
     session.MY_TMP = TemporaryDirectory()
 
-    session.SAMPLE_MP4 = download(
-        "https://download.samplelib.com/mp4/sample-5s.mp4",
-        Path(session.MY_TMP.name) / "sample.mp4",
-    )
-    session.SAMPLE_GALLERY = Path(session.MY_TMP.name) / "gallery"
-    download(
-        "https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg",
-        session.SAMPLE_GALLERY / "01.jpg",
-    )
-    download(
-        "https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg",
-        session.SAMPLE_GALLERY / "02.jpg",
-    )
-    download(
-        "https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg",
-        session.SAMPLE_GALLERY / "subfolder" / "01.jpg",
-    )
+    if getenv("SKIP_DOWNLOAD", "0") == "0":
+        session.SAMPLE_MP4 = download(
+            "https://download.samplelib.com/mp4/sample-5s.mp4",
+            Path(session.MY_TMP.name) / "sample.mp4",
+        )
+        session.SAMPLE_GALLERY = Path(session.MY_TMP.name) / "gallery"
+        download(
+            "https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg",
+            session.SAMPLE_GALLERY / "01.jpg",
+        )
+        download(
+            "https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg",
+            session.SAMPLE_GALLERY / "02.jpg",
+        )
+        download(
+            "https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg",
+            session.SAMPLE_GALLERY / "subfolder" / "01.jpg",
+        )
 
 
 def pytest_sessionfinish(session, exitstatus):
